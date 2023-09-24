8bec76136fe196b89a6121b8321a1eac848327ad
zgmfx20ar <55775578+ZGMFX20AR@users.noreply.github.com>
2023-09-24 11:50:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,25 +16,38 @@ oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
 while True:
     reading = sensor_temp.read_u16() * conversion_factor
-    t= 27 - (reading - 0.706)/0.001721
+    t = 27 - (reading - 0.706) / 0.001721
     oled.fill(0)
-        
+
     formatted_temperature = "{:.1f}".format(t)
-    
+
     font_writer = writer.Writer(oled, freesans20)
-    
+
     font_writer.set_textpos(44, 0)
     font_writer.printstring("Case")
-    
+
     font_writer.set_textpos(8, 20)
     font_writer.printstring("Temperature")
-    
+
     font_writer.set_textpos(46, 44)
     font_writer.printstring(str(formatted_temperature))
-    
+
     font_writer.set_textpos(68, 44)
     font_writer.printstring("C")
-    oled.show() 
-    utime.sleep(1)
+    oled.show()
     
+    # Delay for 5 seconds
+    utime.sleep(3)
+    oled.fill(0)
+
+    # Display another text after the delay
+    font_writer.set_textpos(40, 8)
+    font_writer.printstring("ASRI")
+    
+    font_writer.set_textpos(8, 38)
+    font_writer.printstring("RUSLAN-PC")
+    oled.show()
+    
+    # Delay for 5 seconds again
+    utime.sleep(3)
 
